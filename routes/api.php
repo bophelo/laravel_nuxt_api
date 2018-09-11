babcbaf5d375153758f4a5796ded372a01db6314
Bophelo <mpilo@vivaldi.net>
2018-09-11 05:54:47 +0200

    Siging In + CORS

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@
 Route::post('/register', 'Auth\RegisterController@register');
 Route::post('/login', 'Auth\LoginController@login');
 Route::get('/me', 'Auth\UserController@me');
+Route::post('logout', 'Auth\LoginController@logout');
